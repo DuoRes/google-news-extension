7b38944f64466515d0f4fe9cf1f7f438c1bd4df7
Yueheng Zhang <azicon@berkeley.edu>
2023-03-05 02:17:50 -0800

    beautify popup

--- frontend/src/popup/popup.js
+++ frontend/src/popup/popup.js
@@ -2,13 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
   var loginButton = document.getElementById("login");
 
   loginButton.addEventListener("click", function () {
-    var email = document.getElementById("email").value;
+    var identifier = document.getElementById("identifier").value;
     chrome.runtime.sendMessage(
-      { type: "login", email: email },
+      { type: "login", identifier: identifier },
       function (response) {
-        if (response.success) {
+        if (response?.success) {
           // The user's email address was saved
-          window.close();
+          // redirect to the dashboard
+          window.location.href = "redirect.html";
         } else {
           // There was an error
           alert("There was an error saving your email address.");
