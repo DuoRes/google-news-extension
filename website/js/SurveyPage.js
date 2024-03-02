5870d9771cd1c9a317ad0bf11dc6f696ff1216c6
nociza <azicon@berkeley.edu>
2024-03-01 16:59:49 -0800

    Refactor login function in SurveyPage.js

--- website/js/SurveyPage.js
+++ website/js/SurveyPage.js
@@ -1,12 +1,14 @@
-function login(e){
-    var Indentifier=$("#Indentifier").val();
-    if(Indentifier){
-        if(Indentifier=="thank_you_for_completing_the_haas_research_pre_survey"){
-            window.location.href="./index.html";
-        }else{
-            alert("You have entered the wrong identifier.");
-        }
-    }else{
-        alert("The Unique Indentifier cannot be empty!");
+function login(e) {
+  var Indentifier = $("#Indentifier").val();
+  if (Indentifier) {
+    if (
+      Indentifier == "thank_you_for_completing_the_haas_research_pre_survey"
+    ) {
+      window.location.href = "./Login.html";
+    } else {
+      alert("You have entered the wrong identifier.");
     }
+  } else {
+    alert("The Unique Indentifier cannot be empty!");
+  }
 }
