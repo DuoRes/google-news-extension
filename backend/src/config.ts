6282804671864ebca4266788bec90f24f259cc0e
nociza <azicon@berkeley.edu>
2025-02-02 14:40:32 -0800

    feat: Update EXPERIMENT_BATCH from pilot-11 to pilot-12

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -17,6 +17,6 @@ const config: Config = {
   openaiApiKey: process.env.OPENAI_API_KEY,
 };
 
-export const EXPERIMENT_BATCH = "pilot-11";
+export const EXPERIMENT_BATCH = "pilot-12";
 
 export default config;
